a2b1c6e1eb5f28df99b6bf5d11d28e9aea04be37
Caonabo Batista <caonabojbd@gmail.com>
2026-07-28 11:14:53 -0400

    Commit

--- obj/Debug/net10.0/SistemaFacturacion.AssemblyInfo.cs
+++ obj/Debug/net10.0/SistemaFacturacion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaFacturacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57f0bcea8966362c33eb06c53bff133a969d1853")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaFacturacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaFacturacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
